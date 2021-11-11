7cee058b59f1640de570755462406c9d6d4b1d4d
ibrahimkemalkoyuncu <ibrahimkemalkoyuncu@gmail.com>
2021-11-11 16:40:48 +0300

    core

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/EventBusConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base
+{
+    // EventBusBase projesini kullanarak Subscription yapmak isteyen hangi ServiceBus ı kullanmak isteyecekler
+    // RabbitMQ mu Kafka mı veya Azure service bus mı kullansınlar
+    // //örneğin dışardan bir ConnectionString parametresi alacaklar mı?
+    // Kaç kere bağlanacak gibi ayarlar buradan yapılacak
+
+
+    public class EventBusConfig
+    {
+
+    }
+}
